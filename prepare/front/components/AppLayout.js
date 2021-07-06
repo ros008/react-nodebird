757ab6c333a605a878c41bdc4e61f162cc3cce85
sujin <sujin.k0402@gmail.com>
2021-07-06 09:04:02 +0900

    헤더 추가

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,22 +1,48 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
+import { Menu, Input, Row, Col } from "antd";
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>
-        <Link href="/">
-          <a>노드버드</a>
-        </Link>
-        <Link href="/profile">
-          <a>프로필</a>
-        </Link>
-        <Link href="/signup">
-          <a>회원가입</a>
-        </Link>
-      </div>
-      {children}
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">
+            <a>노드버드</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">
+            <a>회원가입</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽 메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://www.notion.so/next-16a685e9ea2f4649be028dae221ee1ec"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Made By Sujin
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,11 +1,17 @@
 import React from "react";
 import AppLayout from "../components/AppLayout";
+import Head from "next/head";
 
 const Profile = () => {
   return (
-    <AppLayout>
-      <div>내 프로필</div>
-    </AppLayout>
+    <>
+      <AppLayout>
+        <Head>
+          <title>내 프로필 | NodeBird</title>
+        </Head>
+        <div>내 프로필</div>
+      </AppLayout>
+    </>
   );
 };
 
